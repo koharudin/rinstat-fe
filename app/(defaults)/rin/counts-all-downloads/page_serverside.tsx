aa40938982c2c3f17cb1ca8d98465e8ac4203084
Koharudin <koha001>
2025-02-23 21:59:34 +0700

    counts all download clientSide vs serverSide

---
+++ app/(defaults)/rin/counts-all-downloads/page_serverside.tsx
@@ -0,0 +1,40 @@
+import IconBell from '@/components/icon/icon-bell';
+
+export default async () => {
+    const getData = async () => {
+        try {
+            // Melakukan request ke endpoint API
+            console.log('fetching to  ' + process.env.BACKEND_URL);
+            const response = await fetch(process.env.BACKEND_URL + '/rin/get-all-count-downloads');
+
+            // Mengecek apakah respons statusnya 200 OK
+            if (response.status === 200) {
+                const data = await response.json(); // Mengambil data dari respons sebagai JSON
+                console.log('Data received:', data);
+                return data; // Mengembalikan data jika berhasil
+            } else {
+                console.error('Failed to fetch data. Status code:', response.status);
+                return null; // Kembalikan null jika status bukan 200
+            }
+        } catch (error) {
+            // Menangani error jika terjadi kesalahan pada request
+            console.error('Error fetching data:', error);
+            return null;
+        }
+    };
+    const data = await getData();
+    return (
+        <div>
+            <div className="panel flex items-center overflow-x-auto whitespace-nowrap p-3 text-primary">
+                <div className="rounded-full bg-primary p-1.5 text-white ring-2 ring-primary/30 ltr:mr-3 rtl:ml-3">
+                    <IconBell />
+                </div>
+                <span className="ltr:mr-3 rtl:ml-3">Documentation: </span>
+                <a href="https://www.npmjs.com/package/mantine-datatable" target="_blank" className="block hover:underline" rel="noreferrer">
+                    https://www.npmjs.com/package/mantine-datatable
+                </a>
+            </div>
+            <div className="panel mt-6">Data {data?.total}</div>
+        </div>
+    );
+};
